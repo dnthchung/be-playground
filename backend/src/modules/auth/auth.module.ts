b19ba5953b738b8429c6d3efb9e04e995ae85edd
dnthchung <doanthanhchung03nb@gmail.com>
2024-12-15 22:54:03 +0700

    mdf

--- backend/src/modules/auth/auth.module.ts
+++
@@ -1,8 +0,0 @@
-//path : backend/src/modules/auth/auth.module.ts
-import { AuthController } from "./auth.controller";
-import { AuthService } from "./auth.service";
-
-const authService = new AuthService();
-const authController = new AuthController(authService);
-
-export { authService, authController };
