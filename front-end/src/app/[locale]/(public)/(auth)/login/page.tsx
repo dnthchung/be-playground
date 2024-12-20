3f46ef296875ba5b7316b98db93614a0ff27408e
dnthchung <doanthanhchung03nb@gmail.com>
2024-12-21 01:35:01 +0700

    refactor: improve code formatting and update README with installation instructions

--- front-end/src/app/[locale]/(public)/(auth)/login/page.tsx
+++ front-end/src/app/[locale]/(public)/(auth)/login/page.tsx
@@ -1,35 +1,27 @@
-import LoginForm from '@/app/[locale]/(public)/(auth)/login/login-form'
-import Logout from '@/app/[locale]/(public)/(auth)/login/logout'
-import envConfig, { Locale } from '@/config'
-import { getTranslations, unstable_setRequestLocale } from 'next-intl/server'
+import LoginForm from "@/app/[locale]/(public)/(auth)/login/login-form";
+import Logout from "@/app/[locale]/(public)/(auth)/login/logout";
+import envConfig, { Locale } from "@/config";
+import { getTranslations, setRequestLocale } from "next-intl/server";
 
-export async function generateMetadata({
-  params: { locale }
-}: {
-  params: { locale: Locale }
-}) {
-  const t = await getTranslations({ locale, namespace: 'Login' })
-  const url = envConfig.NEXT_PUBLIC_URL + `/${locale}/login`
+export async function generateMetadata({ params: { locale } }: { params: { locale: Locale } }) {
+  const t = await getTranslations({ locale, namespace: "Login" });
+  const url = envConfig.NEXT_PUBLIC_URL + `/${locale}/login`;
 
   return {
-    title: t('title'),
-    description: t('description'),
+    title: t("title"),
+    description: t("description"),
     alternates: {
-      canonical: url
-    }
-  }
+      canonical: url,
+    },
+  };
 }
 
-export default function Login({
-  params: { locale }
-}: {
-  params: { locale: string }
-}) {
-  unstable_setRequestLocale(locale)
+export default function Login({ params: { locale } }: { params: { locale: string } }) {
+  setRequestLocale(locale);
   return (
-    <div className='min-h-screen flex items-center justify-center'>
+    <div className="min-h-screen flex items-center justify-center">
       <LoginForm />
       <Logout />
     </div>
-  )
+  );
 }
--- my-fe-base/src/app/[locale]/guest/layout.tsx
+++ my-fe-base/src/app/[locale]/guest/layout.tsx
@@ -1,12 +1,10 @@
-import Layout from '@/app/[locale]/(public)/layout'
-import { defaultLocale } from '@/config'
+import Layout from "@/app/[locale]/(public)/layout";
+import { defaultLocale } from "@/config";
 
-export default function GuestLayout({
-  children
-}: Readonly<{ children: React.ReactNode }>) {
+export default function GuestLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <Layout modal={null} params={{ locale: defaultLocale }}>
       {children}
     </Layout>
-  )
+  );
 }
